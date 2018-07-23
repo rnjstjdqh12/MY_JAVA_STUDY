51cbb5c62e08682d6d856b4e446bca39efca4acc
admin <admin@10.137.138.158>
2018-07-23 12:22:49 +0900

    Java의 주석이 있음 // /* ~ */ /** ~ **/

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -3,6 +3,17 @@ package icehs.science.chapter02;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
+		/**
+		 * java야 놀자
+		 * @param args
+		 */
+		/*
+		 * 이 프로그램은 선인고등학교 java 응용프로그램은 수업에서 만들어졌습니다.
+		 * 만든사람: 권성보
+		 * 만든날짜: 2018년 7월 23일
+		 * 버전: 0.1
+		 */
+		//나의 첫번째 프포그램
 		System.out.println("Hello World!");
 		System.out.println("Hello rnjstjdqh12");
 	}
